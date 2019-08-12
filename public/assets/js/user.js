c0495f1dde0db759cebedc11fd7c0dd76ed28bcd
Hai-jiang <1138924986@qq.com>
2019-08-12 21:30:06 +0800

    用户删除和批量删除功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -92,4 +92,86 @@ $(function () {
         // 阻止表单默认提交
         return false
     })
+
+
+    // 当删除按钮被点击的时候
+    $('#userBox').on('click', '.delete', function () {
+        if (confirm('您确认要删除该用户吗')) {
+            // 获取到即将要删除的用户id
+            var id = $(this).attr('data-id')
+            // 向服务器端发送请求,删除用户
+            $.ajax({
+                type: 'delete',
+                url: '/users/' + id,
+                success: function () {
+                    location.reload()
+                }
+            })
+        }
+    })
+
+    // 获取全选按钮
+    var selectAll = $('#selectAll')
+    // 获取批量删除按钮
+    var deleteMany = $('#deleteMany')
+
+    // 当全选按钮的状态发生改变时
+    selectAll.on('change', function () {
+        // 获取到全选按钮当前状态
+        var status = $(this).prop('checked')
+
+        if (status) {
+            // 显示批量删除按钮
+            deleteMany.show()
+        } else {
+            deleteMany.hide()
+        }
+
+        // 获取到所有用户并将用户的状态和全选按钮保持一致
+        $('#userBox').find('input').prop('checked', status)
+    })
+
+    // 当用户前面的复选框状态发生改变时
+    $('#userBox').on('change', '.userStatus', function () {
+        // 获取所有用户,对所有用户进行过滤
+        // 判断选中的用户数量和所有用户的数量是否一致
+        // 一致说明全选,否则说明不是全选
+        var inputs = $('#userBox').find('input')
+
+        if (inputs.lenght == inputs.filter(':checked').length) {
+            selectAll.prop('checked', true)
+        } else {
+            selectAll.prop('checked', false)
+        }
+
+        // 如果选中的复选框的数量大于0,就说明有选中的复选框
+        if (inputs.filter(':checked').length > 0) {
+            // 显示批量删除按钮
+            deleteMany.show()
+        } else {
+            deleteMany.hide()
+        }
+    })
+
+    // 为批量删除按钮添加点击事件
+    deleteMany.on('click', function () {
+        var ids = []
+        // 获取选中的用户
+        var checkedUser = $('#userBox').find('input').filter(':checked')
+
+        // 循环复选框,从复选框元素的身上获取data-id的属性的值
+        checkedUser.each(function (index, element) {
+            ids.push($(element).attr('data-id'))
+        })
+
+        if (confirm('您确认要进行批量删除操作吗')) {
+            $.ajax({
+                type: 'delete',
+                url: '/users/' + ids.join('-'),
+                success: function () {
+                    location.reload()
+                }
+            })
+        }
+    })
 })
