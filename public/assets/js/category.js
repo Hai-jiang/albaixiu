28fb2a3cea3645e924a202eefb34767e5cac6eed
Hai-jiang <1138924986@qq.com>
2019-08-14 11:14:49 +0800

    分类功能

---
+++ public/assets/js/category.js
@@ -0,0 +1,77 @@
+$(function () {
+    $('#addCategory') .on('click', function () {
+        var formData = $(this).serialize()
+
+        $.ajax({
+            type: 'post',
+            url: '/categories',
+            data: formData,
+            success: function () {
+                location.reload()
+            }
+        })
+        // 阻止表单默认提交
+        return false
+    })
+
+    // 发送ajax请求,向服务器端所有分类列表数据
+    $.ajax({
+        type: 'get',
+        url: '/categories',
+        success: function (response) {
+            // 将服务器端返回的数据和HTML模板进行拼接
+            var html = template('categoryListTpl', {data: response})
+            // 将拼接好的内容放到页面中
+            $('#categoryBox').html(html)
+        }
+    })
+
+    // 为编辑按钮添加点击事件
+    $('#categoryBox').on('click', '.edit', function () {
+        // 获取要修改的分类数据的id
+        var id = $(this).attr('data-id')
+        // 根据id获取分类数据的详细信息
+        $.ajax({
+            typr: 'get',
+            url: '/categories/' + id,
+            success: function (response) {
+                var html = template('modifyCategoryTpl', response)
+                $('#formBox').html(html)
+            }
+        })
+    })
+
+    // 发送请求,修改分类数据
+    $('#formBox').on('submit', '#modifyCategory', function () {
+        // 获取管理员在表单中输入的内容
+        var formData = $(this).serialize()
+        var id = $(this).attr('data-id')
+        // 发送请求,修改分类数据
+        $.ajax({
+            type: 'put',
+            url: '/categories/' + id,
+            data: formData,
+            success: function () {
+                location.reload()
+            }
+        })
+        // 阻止表单默认提交行为
+        return false
+    })
+
+    // 当删除按钮被点击的时候
+    $('#categoryBox').on('click', '.delete', function () {
+        if (confirm('您真的要执行删除操作吗')) {
+            // 获取要删除的分类数据id
+            var id = $(this).attr('data-id')
+            // 向服务器端发送请求,删除分类数据
+            $.ajax({
+                type: 'delete',
+                url: '/categories/' + id,
+                success: function () {
+                    location.reload()
+                }
+            })
+        }
+    })
+})
